0be94748dd6bfcaeb4fc919663cce313e0d5a844
Thomas Swonke <thomas.swonke@webfactor.de>
2018-04-15 11:49:39 +0200

    [EH] add traits to TextField

--- src/Fields/TextField.php
+++ src/Fields/TextField.php
@@ -3,8 +3,14 @@
 namespace Webfactor\Laravel\Backpack\FluentSyntax\Fields;
 
 use Webfactor\Laravel\Backpack\FluentSyntax\Contracts\CrudFieldAbstract;
+use Webfactor\Laravel\Backpack\FluentSyntax\Traits\Defaultable;
+use Webfactor\Laravel\Backpack\FluentSyntax\Traits\Hintable;
+use Webfactor\Laravel\Backpack\FluentSyntax\Traits\Prefixable;
+use Webfactor\Laravel\Backpack\FluentSyntax\Traits\Suffixable;
 
 class TextField extends CrudFieldAbstract
 {
+    use Prefixable, Suffixable, Defaultable, Hintable;
+
     protected $type = 'text';
 }
