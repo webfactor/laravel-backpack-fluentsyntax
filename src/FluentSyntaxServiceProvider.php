2cb7eae8d145ce2387a437a124ae493afeaf281f
Thomas Swonke <thomas.swonke@webfactor.de>
2018-07-08 00:26:57 +0200

    remove helper methods and fix imports

--- src/FluentSyntaxServiceProvider.php
+++ src/FluentSyntaxServiceProvider.php
@@ -3,10 +3,6 @@
 namespace Webfactor\Laravel\Backpack\FluentSyntax;
 
 use Illuminate\Support\ServiceProvider;
-use Webfactor\Laravel\Generators\Commands\MakeBackpackCrudController;
-use Webfactor\Laravel\Generators\Commands\MakeBackpackCrudModel;
-use Webfactor\Laravel\Generators\Commands\MakeBackpackCrudRequest;
-use Webfactor\Laravel\Generators\Commands\MakeEntity;
 
 class FluentSyntaxServiceProvider extends ServiceProvider
 {
