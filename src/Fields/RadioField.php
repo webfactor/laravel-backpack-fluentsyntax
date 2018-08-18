a5f2533eaa9d8e0db4bbf9aec383521592c2d314
Abby <AbbyJanke@users.noreply.github.com>
2018-08-18 16:53:22 -0700

    Add RadioField

---
+++ src/Fields/RadioField.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Webfactor\Laravel\Backpack\FluentSyntax\Fields;
+
+use Webfactor\Laravel\Backpack\FluentSyntax\Contracts\CrudFieldAbstract;
+use Webfactor\Laravel\Backpack\FluentSyntax\Traits\Defaultable;
+use Webfactor\Laravel\Backpack\FluentSyntax\Traits\Prefixable;
+use Webfactor\Laravel\Backpack\FluentSyntax\Traits\Suffixable;
+
+class RadioField extends CrudFieldAbstract
+{
+    use Hintable, Defaultable;
+
+    protected $type = 'radio';
+
+    public function options(array $options)
+    {
+      $this->options['options'] = $options;
+
+      return $this;
+    }
+
+    public function addOption($value, $key)
+    {
+      $this->options['options'][$value] = $key;
+
+      return $this;
+    }
+
+}
